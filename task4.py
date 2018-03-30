c11a8cba0bd95a2c495f2fb9018d110d30d4be23
Hassaan <muhammadhassaanfarooqui@gmail.com>
2018-03-30 19:49:50 -0400

    task2, 3, 4 done

--- task4.py
+++ task4.py
@@ -5,17 +5,21 @@ from operator import add
 import sys
 
 def printer(res):
-	return ("%s\t%.2f, %.2f" % (res[0], res[1][1], res[1][1]/res[1][0]))
+	return ("%s\t%d" % (res[0], res[1]))
 
-def reducer(pair1, pair2):
-	return((pair1[0] + pair2[0], pair1[1] + pair2[1]))
+def mapper(line):
+    regState = line[16]
+    if ('NY' in regState.upper()):
+        return ('NY', 1)
+    else:
+        return ('Other', 1)
 
 def main(sc,filename):
     lines = sc.textFile(filename)
     lines = lines.mapPartitions(lambda x: reader(x))
-    result = lines.map(lambda line: (line[2], (1, float(line[12])))).reduceByKey(lambda pair1, pair2: reducer(pair1, pair2))
+    result = lines.map(lambda line: mapper(line)).reduceByKey(lambda x, y: x+y)
     result = result.map(lambda res: printer(res));
-    result.saveAsTextFile("task3.out")
+    result.saveAsTextFile("task4.out")
 
 if __name__ == "__main__":
     sc = SparkContext()
